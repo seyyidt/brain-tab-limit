9df800eb0f863ee8b8d2a38cfe715db45ac81031
seyyid <seyyid.tiryaki@gmail.com>
2025-04-29 22:22:11 +0200

    fix: remove alert

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.tabs.onCreated.addListener(async () => {
   let tabs = await chrome.tabs.query({});
-  if (tabs.length > 7) { // Set your tab limit here
+  if (tabs.length > 7) {  
     chrome.tabs.remove(tabs[tabs.length - 1].id);
-    alert("Tab limit reached!");
   }
 });
